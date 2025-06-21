1784f46213e87bf4e349ff54b157b6b95bc4a6f2
sudeepgouda26 <sudeepgouda26@gmail.com>
2025-06-21 23:57:07 +0530

    react 06 practice

---
+++ React_05/src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const App = () => {
+  const  data =["apple", "banana", "orange", "grape", "kiwi", "mango", "peach", "pear", "pineapple", "strawberry"];
+  return (
+    <div>
+   {data.map((value,index)=>(
+     <h2 key={index}>{value}</h2>
+
+   ))}
+
+   <h2>hello</h2>
+    </div>
+  )
+}
+
+export default App
